107aa47f3dd4afd5ca3db2791a0ae95422aa6193
Dennis Gosnell <cdep.illabout@gmail.com>
2021-07-15 14:02:21 +0900

    Get update.sh working locally.

--- update.sh
+++ update.sh
@@ -3,9 +3,10 @@
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz
 #! nix-shell -p hydra-unstable
 #! nix-shell -p "writers.writeHaskellBin \"hydra-report\" {libraries = with haskellPackages; [aeson req];} (path + \"/maintainers/scripts/haskell/hydra-report.hs\")"
+#! nix-shell -p "writers.writeBashBin \"get-nixpkgs-path\" \"echo \${path}\""
 
 set -u -e
 
 hydra-report get-report
-hydra-report ping-maintainers > README.md
+(cd "$(get-nixpkgs-path)" && hydra-report ping-maintainers) > README.md
 cat README-footer.md >> README.md
