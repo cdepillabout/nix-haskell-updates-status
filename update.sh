004335a9739faa6ed8f39d80d63967437abdb591
Dennis Gosnell <cdep.illabout@gmail.com>
2021-12-27 15:03:14 +0900

    Use hydra-unstable from 21.11 since hydra-unstable is broken on master

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz
-#! nix-shell -p hydra-unstable
+#! nix-shell -p "with import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-21.11.tar.gz) {}; hydra-unstable"
 #! nix-shell -p "writers.writeHaskellBin \"hydra-report\" {libraries = with haskellPackages; [aeson req];} (path + \"/maintainers/scripts/haskell/hydra-report.hs\")"
 #! nix-shell -p "writers.writeBashBin \"get-nixpkgs-path\" \"echo \${path}\""
 
@@ -19,6 +19,10 @@
 # has been downloaded for the nix-shell -I argument.  This is somewhat of a hack.
 # I wouldn't be surprised if there wasn't an easier way to get the path to the
 # nixpkgs repo from within nix-shell.
+#
+# TODO: This uses `hydra-unstable` from 21.11 because `hydra-unstable` is broken
+# on `master` as of 2021-12-17:
+# https://github.com/NixOS/nixpkgs/issues/152319
 
 set -u -e
 
